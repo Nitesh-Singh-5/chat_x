0f2ff750cef774912c652f63282f7b2888d0c7ee
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-06-14 21:45:05 +0530

    added react chat engine

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { chatEngine } from 'react-chat-engine';
+import './App.css';
+
+import React from 'react'
+
+export default function App() {
+    return (
+        <chatEngine
+            height="100vh"
+            projectID="8358b0d4-bd62-4a73-94a5-30b137b3cd0d"
+            userName="nitesh58"
+            userSecret="Nitesh@5801"
+        />
+    );
+}
