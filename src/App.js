08b2d5e8413ee2305732ad0059fb03c1b42a572e
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-06-14 22:09:26 +0530

    added Chat Feed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { chatEngine } from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
 
 import React from 'react'
 
 export default function App() {
     return (
-        <chatEngine
+        <ChatEngine
             height="100vh"
             projectID="8358b0d4-bd62-4a73-94a5-30b137b3cd0d"
             userName="nitesh58"
             userSecret="Nitesh@5801"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ChatFeed(props) {
+    console.log(props);
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
+}
