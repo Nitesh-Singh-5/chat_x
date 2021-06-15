7dd274a198bb2dd81f0cb394ffa4003f7e59cbb0
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-06-15 22:17:47 +0530

    added chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 export default function ChatFeed(props) {
-    console.log(props);
+    const {chats, activeChat, userName, messages} = props;
+    const chat = chats && chats[activeChat];
+    
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        return keys.map((key,index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMymessage = userName === message.sender.username;
+            return (
+                <div key={`msg_${index}`} style={{width:'100%'}}>
+                    <div className="message-block">
+                        {
+                            isMymessage
+                            ?<MyMessage message={message}/>
+                            :<TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
+                        }
+                    </div>
+                    <div className="read-receipt" style={{ marginRight:isMymessage?'18px':'0px',marginLeft:isMymessage?'0px':'68px'}}>
+                    read-receipt
+                    </div>
+                </div>
+            )
+        })
+
+    }
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div>
+                    <div className="chat-title">{chat.title}</div>
+                    <div className="chat-subtitle">
+                        {chat.people.map((person) => `${person.person.username}`)}
+                    </div>
+                </div>
+                {renderMessages()}
+                <div style={{height:'100px'}}></div>
+                <div className="message-form-container">
+                    <MessageForm {...props} chatId={activeChat}/>
+                </div>
+            </div>
         </div>
     )
 }
